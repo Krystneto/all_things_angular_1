0481f9fba511ba4a7f15b13b8faeed395036a9b7
Kristopher Parrillo <kkparrillo@gmail.com>
2017-02-23 17:37:37 -0800

    working presidents

--- angular_http/starter_code/public/controllers/presidentsController.js
+++ angular_http/starter_code/public/controllers/presidentsController.js
@@ -5,22 +5,64 @@
     .module('ThePresidentsApp')
     .controller('PresidentsController', PresidentsController);
 
-  PresidentsController.$inject = [];
+  PresidentsController.$inject = ['$http'];
 
-  function PresidentsController(){
+  function PresidentsController($http){
     var vm = this;
-    vm.all = [
-      {"name": "Blorp Florp McRichards", "start": 1789, "end": 1790 },
-      {"name": "John MuscleBrain Adams", "start": 1790, "end": 1801 },
-      {"name": "Blogpost Dorgabn", "start": 1801, "end": 1949 },
-      {"name": "Mike", "start": 1949, "end": 1947 }
-    ];
+    //   {"name": "Blorp Florp McRichards", "start": 1789, "end": 1790 },
+    //   {"name": "John MuscleBrain Adams", "start": 1790, "end": 1801 },
+    //   {"name": "Blogpost Dorgabn", "start": 1801, "end": 1949 },
+    //   {"name": "Mike", "start": 1949, "end": 1947 }
+    // ];
     vm.addPresident = addPresident;
     vm.newPresident = {};
+    vm.all = []
+    getPresidents();
+    vm.removePresident = removePresident;
+    vm.uncover = uncover;
 
-    function addPresident(){
-      vm.all.push(vm.newPresident);
-      vm.newPresident = {};
+    function getPresidents () {
+      $http
+      .get('/api/presidents')
+      .then(function( response ) {
+        vm.all = response.data.presidents;
+      }, function(err) {
+          console.log(err)
+      })
     }
+
+    function addPresident() {
+      $http
+      .post('/api/presidents', vm.newPresident)
+      .then(function(res) {
+        vm.all.push(vm.newPresident);
+        vm.newPresident = {};
+      }, function(err) {
+        console.log(err)
+      })
+    }
+
+    function removePresident(id) {
+      console.log(id)
+      $http
+      .delete('/api/presidents/' + id)
+      .then(function(res) {
+        console.log(res)
+      }, function(err) {
+        console.log(err)
+      })
+    }
+
+    function uncover(id) {
+      console.log(id)
+      $http
+      .put('/api/presidents/'+ id, {uncovered: true} )
+      .then(function(res) {
+        console.log(res)
+      }, function(err) {
+        console.log(err)
+      })
+    }
+
   }
 })();
